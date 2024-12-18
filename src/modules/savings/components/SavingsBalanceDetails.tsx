770770316efaf1218cb1d381a2b871d23b2b48ca
inclinedmind <138821869+inclinedmind@users.noreply.github.com>
2024-12-17 20:25:56 -1000

    fix layout for base

--- src/modules/savings/components/SavingsBalanceDetails.tsx
+++ src/modules/savings/components/SavingsBalanceDetails.tsx
@@ -8,35 +8,64 @@ export function SavingsBalanceDetails() {
   const chainId = useChainId();
   const { address } = useAccount();
   const { data, isLoading, error } = useSavingsData();
+  const isBase = isBaseChainId(chainId);
   const { data: usdcBalance } = useTokenBalance({
     chainId,
     address,
     token: usdcBaseAddress[chainId as keyof typeof usdcBaseAddress],
-    enabled: isBaseChainId(chainId)
+    enabled: isBase
   });
-  console.log('usdcBalance', usdcBalance);
+
   const usdsToken = { name: 'USDS', symbol: 'USDS' };
   const usdcToken = { name: 'USDC', symbol: 'USDC', decimals: 6 };
-  return (
-    <div className="flex w-full flex-col justify-between gap-3 xl:flex-row">
+
+  const SuppliedSavingsBalanceCard = () => {
+    return (
       <SuppliedBalanceCard
         balance={data?.userSavingsBalance || 0n}
         isLoading={isLoading}
         token={usdsToken}
         error={error}
       />
+    );
+  };
+
+  const UsdsBalanceCard = () => {
+    return (
       <UnsuppliedBalanceCard
         balance={data?.userNstBalance || 0n}
         isLoading={isLoading}
         token={usdsToken}
         error={error}
       />
+    );
+  };
+
+  const UsdcBalanceCard = () => {
+    return (
       <UnsuppliedBalanceCard
         balance={usdcBalance?.value || 0n}
         isLoading={isLoading}
         token={usdcToken}
         error={error}
       />
+    );
+  };
+
+  return isBase ? (
+    <div className="flex w-full flex-col gap-3">
+      <div className="w-full">
+        <SuppliedSavingsBalanceCard />
+      </div>
+      <div className="flex w-full flex-col justify-between gap-3 xl:flex-row">
+        <UsdsBalanceCard />
+        <UsdcBalanceCard />
+      </div>
+    </div>
+  ) : (
+    <div className="flex w-full flex-col justify-between gap-3 xl:flex-row">
+      <SuppliedSavingsBalanceCard />
+      <UsdsBalanceCard />
     </div>
   );
 }
